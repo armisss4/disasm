cfd96b82fc846bac06c6c09756c5c4a4b3860558
armisss4 <armisss4@gmail.com>
2017-12-11 21:37:05 +0200

    Add files via upload

---
+++ opcodes.asm
@@ -0,0 +1,60 @@
+    .Unknown        db 'NEATPAZINTA',0
+    .mov            db 'mov',0
+    .PushES         db 'push ES',0
+    .PushCS         db 'push CS',0
+    .PushSS         db 'push SS',0
+    .PushDS         db 'push DS',0
+    .PopES          db 'pop ES',0
+    .PopCS          db 'pop CS',0
+    .PopSS          db 'pop SS',0
+    .PopDS          db 'pop DS',0
+    .IncAX          db 'inc AX',0
+    .IncSP          db 'inc SP',0
+    .IncBX          db 'inc BX',0
+    .IncDI          db 'inc DI',0
+    .IncCX          db 'inc CX',0
+    .IncBP          db 'inc BP',0
+    .IncDX          db 'inc DX',0
+    .IncSI          db 'inc SI',0
+    .DecAX          db 'dec AX',0
+    .DecSP          db 'dec SP',0
+    .DecBX          db 'dec BX',0
+    .DecDI          db 'dec DI',0
+    .DecCX          db 'dec CX',0
+    .DecBP          db 'dec BP',0
+    .DecDX          db 'dec DX',0
+    .DecSI          db 'dec SI',0
+    .PushAX         db 'push AX',0
+    .PushSP         db 'push SP',0
+    .PushBX         db 'push BX',0
+    .PushDI         db 'push DI',0
+    .PushCX         db 'push CX',0
+    .PushBP         db 'push BP',0
+    .PushDX         db 'push DX',0
+    .PushSI         db 'push SI',0
+    .PopAX          db 'pop AX',0
+    .PopSP          db 'pop SP',0
+    .PopBX          db 'pop BX',0
+    .PopDI          db 'pop DI',0
+    .PopCX          db 'pop CX',0
+    .PopBP          db 'pop BP',0
+    .PopDX          db 'pop DX',0
+    .PopSI          db 'pop SI',0
+    .Jo             db 'jo',0
+    .Jno            db 'jno',0
+    .Jnae           db 'jnae',0
+    .Jae            db 'jae',0
+    .Je             db 'je',0
+    .Jne            db 'jne',0
+    .Jbe            db 'jbe',0
+    .Ja             db 'ja',0
+    .Js             db 'js',0
+    .Jns            db 'jns',0
+    .Jp             db 'jp',0
+    .Jnp            db 'jnp',0
+    .Jl             db 'jl',0
+    .Jge            db 'jge',0
+    .Jle            db 'jle',0
+    .Jg             db 'jg',0
+    .Jcxz           db 'jcxz',0
+    
