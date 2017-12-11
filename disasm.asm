1790bbbe153b2e17a32a020ade61abe888f9af6b
armisss4 <armisss4@gmail.com>
2017-12-11 23:55:53 +0200

    Update disasm.asm

--- disasm.asm
+++ disasm.asm
@@ -54,6 +54,13 @@ LOCAL @@exit
 @@exit:
 ENDM
 
+Sjc MACRO location
+LOCAL @@exit
+    jnc @@exit
+    jmp location
+@@exit:
+ENDM
+
 OutFill MACRO mcode, acode
     mov ax, @data
     mov es, ax
@@ -69,11 +76,39 @@ OutFill MACRO mcode, acode
     jmp save
 ENDM
 
-Sjc MACRO location
-LOCAL @@exit
-    jnc @@exit
-    jmp location
-@@exit:
+OutFillDisplacement MACRO mcode,mcode2, acode
+LOCAL @@exit, @@less, @@less1, @@more
+    mov ax, @data
+    mov es, ax
+    push bx
+    Pos position, outBuffer
+    ToAscii mcode, outBuffer+7
+    ToAscii mcode2, outBuffer+9
+    Move 68h, outBuffer+4
+    Move 3Ah, outBuffer+5
+    Move 20h, outBuffer+6 
+    MoveStrToBuf acode, outBuffer+24
+    Move 0Dh, outBuffer+98
+    Move 0Ah, outBuffer+99
+    inc position
+    inc position
+    mov bl, mcode2
+    cmp bl, 80h
+    jb @@less1
+    jmp @@more
+    @@less1:
+    jmp @@less
+    @@more:
+    
+    Pos position-bx, outBuffer+si+1
+    jmp @@exit
+    @@less:
+    Pos position+bx, outBuffer+si+1    
+    @@exit:
+    pop si
+    inc si
+    pop bx
+    jmp save
 ENDM
 
 Move MACRO a,b
@@ -87,7 +122,7 @@ MoveStrToBuf MACRO a,b
 LOCAL @@start,@@exit
     push ax
     push si
-    mov si, 00h
+    xor si, si
     @@start:
     mov ah, a[si]
     cmp ah, 00h
@@ -96,7 +131,6 @@ LOCAL @@start,@@exit
     inc si
     jmp @@start
     @@exit:
-    pop si
     pop ax
 ENDM
 
@@ -114,7 +148,7 @@ ToAscii MACRO a,b
     push ax
     push bx
     mov al, a
-    mov ah, 0h
+    xor ah, ah
     mov bl, 10h
     div bl
     mov b, al
@@ -165,7 +199,7 @@ ENDM
 Clr MACRO buffer 
 LOCAL @@start, @@exit
     push Si
-    mov Si, 00h
+    xor Si, Si
     @@start:
     cmp si,100
     je @@exit
@@ -176,11 +210,15 @@ LOCAL @@start, @@exit
     pop Si
 ENDM
 
-Compare MACRO b, loc
-LOCAL @@exit
+Compare1 MACRO b, acode
+LOCAL @@exit, @@start,@@exit1
 cmp byte ptr ds:[inBuffer+si], b
-jne @@exit
-jmp loc
+jne @@exit1
+jmp @@start
+@@exit1:
+jmp @@exit
+@@start:
+OutFill b, acode
 @@exit:
 ENDM
 
@@ -214,7 +252,7 @@ start:
     mov ds, ax
     mov	dx, offset writeFile
 	mov	ah, 3Ch
-	mov	cx, 00h
+	xor	cx, cx
 	int	21h
     Sjc print_info
     mov ah, 3Dh
@@ -237,7 +275,7 @@ start:
     startCycle:
     inc si
     cmp si,temp
-    Sje reread
+    jae reread
     Clr outBuffer
     call check_byte
     
@@ -264,176 +302,97 @@ space_check ENDP
 
 check_byte PROC near
     check_begin:
-    Compare 06h, PushES
-    Compare 0Eh, PushCS
-    Compare 16h, PushSS
-    Compare 1Eh, PushDS
-    Compare 07h, PopES
-    Compare 0Fh, PopCS
-    Compare 17h, PopSS
-    Compare 1Fh, PopDS
-    Compare 07h, PopES
-    Compare 0Fh, PopCS
-    Compare 17h, PopSS
-    Compare 1Fh, PopDS
-    Compare 40h, IncAX
-    Compare 44h, IncSP
-    Compare 43h, IncBX
-    Compare 47h, IncDI
-    Compare 41h, IncCX
-    Compare 45h, IncBP
-    Compare 42h, IncDX
-    Compare 46h, IncSI
-    Compare 48h, DecAX
-    Compare 4Ch, DecSP
-    Compare 4Bh, DecBX
-    Compare 4Fh, DecDI
-    Compare 49h, DecCX
-    Compare 4Dh, DecBP
-    Compare 4Ah, DecDX
-    Compare 4Eh, DecSI
-    Compare 50h, PushAX
-    Compare 54h, PushSP
-    Compare 53h, PushBX
-    Compare 57h, PushDI
-    Compare 51h, PushCX
-    Compare 55h, PushBP
-    Compare 52h, PushDX
-    Compare 56h, PushSI
-    Compare 58h, PopAX
-    Compare 5Ch, PopSP
-    Compare 5Bh, PopBX
-    Compare 5Fh, PopDI
-    Compare 59h, PopCX
-    Compare 5Dh, PopBP
-    Compare 5Ah, PopDX
-    Compare 5Eh, PopSI
+    Compare1 06h, .PushES
+    Compare1 0Eh, .PushCS
+    Compare1 16h, .PushSS
+    Compare1 1Eh, .PushDS
+    Compare1 07h, .PopES
+    Compare1 0Fh, .PopCS
+    Compare1 17h, .PopSS
+    Compare1 1Fh, .PopDS
+    Compare1 07h, .PopES
+    Compare1 0Fh, .PopCS
+    Compare1 17h, .PopSS
+    Compare1 1Fh, .PopDS
+    Compare1 40h, .IncAX
+    Compare1 44h, .IncSP
+    Compare1 43h, .IncBX
+    Compare1 47h, .IncDI
+    Compare1 41h, .IncCX
+    Compare1 45h, .IncBP
+    Compare1 42h, .IncDX
+    Compare1 46h, .IncSI
+    Compare1 48h, .DecAX
+    Compare1 4Ch, .DecSP
+    Compare1 4Bh, .DecBX
+    Compare1 4Fh, .DecDI
+    Compare1 49h, .DecCX
+    Compare1 4Dh, .DecBP
+    Compare1 4Ah, .DecDX
+    Compare1 4Eh, .DecSI
+    Compare1 50h, .PushAX
+    Compare1 54h, .PushSP
+    Compare1 53h, .PushBX
+    Compare1 57h, .PushDI
+    Compare1 51h, .PushCX
+    Compare1 55h, .PushBP
+    Compare1 52h, .PushDX
+    Compare1 56h, .PushSI
+    Compare1 58h, .PopAX
+    Compare1 5Ch, .PopSP
+    Compare1 5Bh, .PopBX
+    Compare1 5Fh, .PopDI
+    Compare1 59h, .PopCX
+    Compare1 5Dh, .PopBP
+    Compare1 5Ah, .PopDX
+    Compare1 5Eh, .PopSI
+
+    call CompareJ
+
     
-    Compare 0B4h, MovAh
+    ;Compare1 0B4h, MovAh
     
     OutFill ds:[inBuffer+si], .Unknown
     ret
 check_byte ENDP
 
-PushES:
-OutFill 06h, .PushES
-
-PushCS:
-OutFill 0Eh, .PushCS
-
-PushSS:
-OutFill 16h, .PushSS
-
-PushDS:
-OutFill 1Eh, .PushDS
-
-PopES:
-OutFill 07h, .PopES
-
-PopCS:
-OutFill 0Fh, .PopCS
-
-PopSS:
-OutFill 17h, .PopSS
-
-PopDS:
-OutFill 1Fh, .PopDS
-
-IncAX:
-OutFill 40h, .IncAX
-
-IncSP:
-OutFill 44h, .IncSP
-
-IncBX:
-OutFill 43h, .IncBX
-
-IncDI:
-OutFill 47h, .IncDI
-
-IncCX:
-OutFill 41h, .IncCX
-
-IncBP:
-OutFill 45h, .IncBP
-
-IncDX:
-OutFill 42h, .IncDX
-
-IncSI:
-OutFill 46h, .IncSI
-
-DecAX:
-OutFill 48h, .DecAX
-
-DecSP:
-OutFill 4Ch, .DecSP
-
-DecBX:
-OutFill 4Bh, .DecBX
-
-DecDI:
-OutFill 4Fh, .DecDI
-
-DecCX:
-OutFill 49h, .DecCX
-
-DecBP:
-OutFill 4Dh, .DecBP
-
-DecDX:
-OutFill 4Ah, .DecDX
-
-DecSI:
-OutFill 4Eh, .DecSI
-
-PushAX:
-OutFill 50h, .PushAX
-
-PushSP:
-OutFill 54h, .PushSP
-
-PushBX:
-OutFill 53h, .PushBX
-
-PushDI:
-OutFill 57h, .PushDI
-
-PushCX:
-OutFill 51h, .PushCX
-
-PushBP:
-OutFill 55h, .PushBP
-
-PushDX:
-OutFill 52h, .PushDX
-
-PushSI:
-OutFill 56h, .PushSI
-
-PopAX:
-OutFill 58h, .PopAX
-
-PopSP:
-OutFill 5Ch, .PopSP
-
-PopBX:
-OutFill 5Bh, .PopBX
-
-PopDI:
-OutFill 5Fh, .PopDI
-
-PopCX:
-OutFill 59h, .PopCX
-
-PopBP:
-OutFill 5Dh, .PopBP
-
-PopDX:
-OutFill 5Ah, .PopDX
-
-PopSI:
-OutFill 5Eh, .PopSI
+CompareJ PROC near
+    cmp byte ptr ds:[inBuffer+si], 70h
+    jae JCombined
+    jmp JCombinedSkip
+    JCombined:
+    cmp byte ptr ds:[inBuffer+si], 7Fh
+    jbe JCombined2
+    jmp JCombinedSkip
+    JCombined2:
+    push ax
+    push bx
+    mov al, ds:[inBuffer+si]
+    xor ah, ah
+    mov bl, 10h
+    div bl
+    mov bx, 00h
+    inc ah
+    JCombined3:
+    dec ah
+    inc bx
+    cmp ah, 00h
+    je JCombined5
+    JCombined4:
+    mov al, [.JCombined+bx]
+    cmp al, 00h
+    je JCombined3
+    inc bx
+    jmp JCombined4
+    JCombined5:
+    OutFillDisplacement ds:[inBuffer+si],ds:[inBuffer+si+1],[.JCombined+bx] 
+    pop bx
+    pop ax
+    
+    
+    JCombinedSkip:
+    ret
+CompareJ ENDP
 
 
 
@@ -445,11 +404,7 @@ save:
     mov ds, ax
     mov bx, writeHandle
     mov cx, 100
-    ;mov dx, inBuffer+2
-    ;Move inBuffer+5,outBuffer+1
- ;   mov outBuffer, inBuffer+2
     mov dx, offset outBuffer
-  ;  mov outBuffer+5, inBuffer
     mov ah, 40h
     int 21h
     jmp startCycle
